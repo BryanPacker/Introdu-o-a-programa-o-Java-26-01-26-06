dc393edd7a36e0b6d94029e3eb40818acf255a6f
Bryan Felipe Packer <bfpacker@furb.br>
2026-03-12 21:39:28 -0300

    aula 12/03/26

---
+++ src/Un3/Exe/Uni3Exe04.java
@@ -0,0 +1,21 @@
+package Un3.Exe;
+
+public class Uni3Exe04 {
+    
+    private float nota1;
+    private float nota2;
+    private float nota3;
+
+    public Uni3Exe04(float nota1, float nota2, float nota3){
+
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+
+    }
+
+    public float calculoPonderada(){
+
+        return this.nota1 * 
+    }
+}
